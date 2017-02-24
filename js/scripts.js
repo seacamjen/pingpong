206be8e10ef1997694f38c4b8561290e3fc59324
Cameron Jensen <jensenseanc@gmail.com>
2017-02-24 11:16:09 -0800

    Add output numbers are displayed on their own lines.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 //Business Logic
 var numberArray = [];
 var sliced = [];
+var list = [];
 
 var count = function(pingpong){
   for (var i = 1; i <= numerials; i++) {
     numberArray.push(i);
-    console.log(numberArray);
   }
-
+  // debugger;
   sliced = numberArray.slice(0, numerials);
   return sliced;
 };
 
-
-// "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+// return sliced;
 
 //User Interface
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-
     numerials = parseInt($("#numberEntry").val());
     output = count(numerials);
+
+    output.forEach(function(nums) {
+      $("ul").append("<li>" + nums + "</li>");
+    });
+    
     $(".answer").show();
-    $("#count").text(output);
   });
 });
